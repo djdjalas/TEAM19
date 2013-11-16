4dec4f942039af42f7051ec3ff16581b69c8ad33
constantinos <cv1212@imperial.ac.uk>
2013-11-16 19:48:00 +0000

    Added buttons

--- gen/com/example/thefoodchain/R.java
+++ gen/com/example/thefoodchain/R.java
@@ -29,7 +29,10 @@ public final class R {
         public static final int logo=0x7f020002;
     }
     public static final class id {
-        public static final int action_settings=0x7f090000;
+        public static final int action_settings=0x7f090003;
+        public static final int button1=0x7f090002;
+        public static final int editText1=0x7f090001;
+        public static final int imageView1=0x7f090000;
     }
     public static final class layout {
         public static final int activity_list_targets=0x7f030000;
