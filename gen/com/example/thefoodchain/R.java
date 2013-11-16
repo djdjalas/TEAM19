ba2986b8cf3e5e1e55d97d8fec70612536824a7c
constantinos <cv1212@imperial.ac.uk>
2013-11-16 21:23:19 +0000

    jkdfksjhdfksjd

--- gen/com/example/thefoodchain/R.java
+++ gen/com/example/thefoodchain/R.java
@@ -32,11 +32,15 @@ public final class R {
         public static final int text_field_main=0x7f020003;
     }
     public static final class id {
-        public static final int action_settings=0x7f090004;
-        public static final int button1=0x7f090003;
-        public static final int editText1=0x7f090001;
-        public static final int imageView1=0x7f090000;
+        public static final int LinearLayout1=0x7f090000;
+        public static final int action_settings=0x7f090008;
+        public static final int button1=0x7f090007;
+        public static final int editText1=0x7f090006;
+        public static final int imageView1=0x7f090001;
+        public static final int listView1=0x7f090005;
         public static final int textView1=0x7f090002;
+        public static final int textView2=0x7f090003;
+        public static final int textView3=0x7f090004;
     }
     public static final class layout {
         public static final int activity_list_targets=0x7f030000;
