96fb1599c57f4cb7344784e3237ca0091b2f05cf
constantinos <cv1212@imperial.ac.uk>
2013-11-16 17:25:47 +0000

    Added some missing files

---
+++ src/com/example/thefoodchain/SingleTarget.java
@@ -0,0 +1,17 @@
+package com.example.thefoodchain;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SingleTarget extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_single_target);
+	}
+	
+	
+
+}
